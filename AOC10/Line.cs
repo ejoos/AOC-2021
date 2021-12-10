3df670d34c57e5c3d17988aad9116ed7b73053b4
Johan Östlund <johan@ejoos.se>
2021-12-10 22:08:37 +0100

    AOC10

---
+++ AOC10/Line.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC10
+{
+    internal class Line
+    {
+        private static readonly Dictionary<char, int> _lefts;
+        private static readonly Dictionary<char, int> _rights;
+
+        internal bool IsCorrupt { get; private set; }
+        internal bool IsIncomplete { get; private set; }
+        internal long Cost { get; private set; }
+
+
+        static Line()
+        {
+            _lefts = new Dictionary<char, int>()
+            {
+                {'(', 1 },
+                {'[', 2 },
+                {'{', 3 },
+                {'<', 4 }
+            };
+             
+            _rights = new Dictionary<char, int>()
+            {
+                {')', 3 },
+                {']', 57 },
+                {'}', 1197 },
+                {'>', 25137 }
+            };
+        }
+
+        internal Line(string data)
+        {            
+            Check(data);
+        }
+
+        private void Check(string data)
+        {
+            var chars = data.ToArray().ToList();
+            var checkedChars = new List<char>();
+            int i;
+            for (i = 0; i < chars.Count; i++)
+            {
+                var c = chars[i];
+                if (IsLeft(c))
+                {
+                    checkedChars.Add(c);
+                }
+                else if (IsRight(c))
+                {
+                    if (!RemoveChunk(checkedChars, c))
+                    {
+                        IsCorrupt = true;
+                        Cost = GetCostRight(c);
+                        break;
+                    }                                       
+                }
+            }
+
+            if (i == chars.Count && checkedChars.Count > 0)
+            {
+                IsIncomplete = true;
+                Cost = GetCompleteCost(checkedChars);                
+            }            
+        }
+
+        internal static long GetMiddleCompletionCost(List<Line> lines)
+        {
+            var costs = new List<long>();
+            foreach (var l in lines.Where(l => l.IsIncomplete))
+            {
+                costs.Add(l.Cost);
+            }
+            costs.Sort();
+            return costs[costs.Count / 2];
+        }
+
+        private static long GetCompleteCost(List<char> checkedChars)
+        {
+            long cost = 0;
+            checkedChars.Reverse();
+            foreach (var c in checkedChars)
+            {
+                cost = cost * 5 + GetCostLeft(c);
+            }
+            return cost;
+        }
+       
+        private static bool RemoveChunk(List<char> checkedChars, char c)
+        {            
+            if (IsPair(checkedChars.Last(), c))
+            {
+                checkedChars.RemoveAt(checkedChars.Count - 1);
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsPair(char l, char r)
+        {
+            return (l == '(' && r == ')') ||
+                   (l == '[' && r == ']') ||
+                   (l == '{' && r == '}') ||
+                   (l == '<' && r == '>');            
+        }
+
+        private static bool IsLeft(char c)
+        {
+            return _lefts.Keys.Contains(c);
+        }
+
+        private static bool IsRight(char c)
+        {
+            return _rights.Keys.Contains(c);
+        }
+        
+        private static int GetCostRight(char c)
+        {
+            return _rights[c];
+        }
+
+        private static int GetCostLeft(char c)
+        {
+            return _lefts[c];
+        }
+
+    }
+}
---
+++ AOC10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
+        {
+            var lineData = File.ReadAllLines(@"C:\AdventOfCode\AOC\AOC10\1.txt").ToList();
+
+            long sum = 0;
+            foreach(var l in lineData)
+            {
+                var line = new Line(l);
+                if (line.IsCorrupt) 
+                { 
+                    sum += line.Cost;
+                }
+            }
+            
+            Console.WriteLine(sum);
+        }
+
+        private static void Part2()
+        {
+            var lineData = File.ReadAllLines(@"C:\AdventOfCode\AOC\AOC10\1.txt").ToList();
+            
+            List<Line> inCompleteLines = new();
+            foreach (var l in lineData)
+            {
+                var line = new Line(l);
+                if (line.IsIncomplete)
+                {
+                    inCompleteLines.Add(line);                    
+                }                
+            }
+
+            long cost = Line.GetMiddleCompletionCost(inCompleteLines);
+            Console.WriteLine(cost);
+        }
+
+    }
+}
