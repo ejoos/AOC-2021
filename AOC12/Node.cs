13c4ce4f31ca4dc6b58cbdc60f08b339c563e86c
Johan Östlund <johan@ejoos.se>
2021-12-12 13:15:31 +0100

    Kommenterat

--- AOC12/Node.cs
+++ AOC12/Node.cs
@@ -25,6 +25,10 @@ namespace AOC12
             List<Node> connNodes = new();
             if (Code != "end")
             {
+                //If...
+                //1: The To node is not small (big nodes are always ok) OR
+                //2: The path doesn´t already contain To node (small and big nodes are always ok if not already in the path) 
+                //..Then its ok
                 Connections.Where(l => !l.To.Small || !p.Contains(l.To)).ToList().ForEach(l => connNodes.Add(l.To));
             }
             return connNodes;
@@ -35,6 +39,11 @@ namespace AOC12
             List<Node> connNodes = new();
             if (Code != "end")
             {
+                //If...
+                //1: The To node is not small (big nodes are always ok) OR
+                //2: The path doesn´t already contain To node (small and big nodes are always ok if not already in the path) OR
+                //3: The path doesn´t contain any small nodes that is visited twice (ok to add small node again)
+                //..Then its ok IF the To node isn´t the start node. 
                 Connections.Where(l => (!l.To.Small || !p.Contains(l.To) || p.NotContainsDoubleVisitedSmall()) && l.To.Code != "start").ToList().ForEach(l => connNodes.Add(l.To));
             }            
             return connNodes;
